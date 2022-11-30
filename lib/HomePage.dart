103dc2abce23be42322cfb37a04709650a44ff77
amirrezaghabeli <ghabeli74@gmail.com>
2022-11-29 16:24:09 -0800

    first project

---
+++ lib/HomePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('آموزش Getx')),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Get.defaultDialog(
+                title: 'dialog',
+                middleText: 'this is getx dialog',
+              );
+            },
+            child: Text('show dialog')),
+      ),
+    );
+  }
+}
